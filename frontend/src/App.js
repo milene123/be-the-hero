fbf7b18b63973b21b9c249340584993278cddefd
Milene <milene.reis944@gmail.com>
2020-04-03 23:58:55 -0300

    Backend da Aplicaçao

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './global.css';
+import Routes from './routes';
+//import Logon from './pages/Logon';
+
+function App() {
+    
+  return (
+   <Routes/>
+  )
+}
+
+export default App;
---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,77 @@
+import React from  'react';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import logoImg from '../../asserts/logo.svg';
+import { FiPower,FiTrash2} from 'react-icons/fi'
+export default function Profile(){
+    return (
+        <div className="profile-container">
+            <section>
+            <header>
+                <img src={logoImg}alt="Be the Hero"/>
+                <span>Bem vindo a APAD</span>
+                <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+                <button type="button">
+                    <FiPower size={18} color= "#E020441"></FiPower>
+                </button>
+            </header>
+            </section>
+            <form>
+            <h1>Casos cadastrados</h1>
+            <ul>
+                <li>
+                    <strong>Caso:</strong>
+                    <p>Caso teste</p>
+                    <strong>Descrição:</strong>
+                    <p>Descrição de teste</p>
+                    <strong>Valor:</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8ab3"/>
+                    </button>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Caso:</strong>
+                    <p>Caso teste</p>
+                    <strong>Descrição:</strong>
+                    <p>Descrição de teste</p>
+                    <strong>Valor:</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8ab3"/>
+                    </button>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Caso:</strong>
+                    <p>Caso teste</p>
+                    <strong>Descrição:</strong>
+                    <p>Descrição de teste</p>
+                    <strong>Valor:</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8ab3"/>
+                    </button>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Caso:</strong>
+                    <p>Caso teste</p>
+                    <strong>Descrição:</strong>
+                    <p>Descrição de teste</p>
+                    <strong>Valor:</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8ab3"/>
+                    </button>
+                </li>
+            </ul>
+
+            </form>
+        </div>
+    );
+}
